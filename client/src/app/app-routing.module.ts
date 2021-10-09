38a81d76d4777f2ba3d891fa44f64077684754e7
ameer-mughal-hamza <hamza6417307@gmail.com>
2021-10-09 05:04:15 +0200

    Google login button css styling and logo

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },{
+    path: 'about',
+    component: HomeComponent
+  },{
+    path: 'contact',
+    component: HomeComponent
   }
 ];
 
